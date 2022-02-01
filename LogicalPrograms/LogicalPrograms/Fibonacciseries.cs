c534488e10ceb5fe88256c77d4a4ff1cda091bc7
Sikander <sikku.rk405@gmail.com>
2022-02-01 16:47:59 +0530

    [Sikander] Added fibonacci series program

---
+++ LogicalPrograms/LogicalPrograms/Fibonacciseries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Fibonacciseries
+    {
+
+        public int a = 0;
+        public int b = 1;
+        public int c;
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter the number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci series : ");
+            Console.WriteLine(a+ "\n" +b);
+            for (int i = 2; i < number; i++)
+            {   
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your choice : ");
+            Console.WriteLine("1 For: Fibonacci series. ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    //Fibonacci Series
+                    Fibonacciseries FibSeries = new Fibonacciseries();
+                    FibSeries.Fibonacci();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
